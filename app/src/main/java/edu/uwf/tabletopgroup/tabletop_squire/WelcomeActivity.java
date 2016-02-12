c7e0fd63a10dbeb4cdb6a531c1ff0cb1b2749255
Quinn Roundy <Quinn Roundy>
2016-02-11 20:54:30 -0600

    Changed things added layouts for stuff having commit/push issues, lost original log of changes

---
+++ app/src/main/java/edu/uwf/tabletopgroup/tabletop_squire/WelcomeActivity.java
@@ -0,0 +1,12 @@
+package edu.uwf.tabletopgroup.tabletop_squire;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
