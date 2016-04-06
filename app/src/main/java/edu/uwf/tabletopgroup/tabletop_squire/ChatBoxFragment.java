18181a91f5cb5662c2f3bf6abf6698bc4dcf2f93
Quinn Roundy <qjr3@students.uwf.edu>
2016-04-06 10:58:38 -0500

    Chat Room, fragments, SocketIO and more commit before pull/merge

--- app/src/main/java/edu/uwf/tabletopgroup/tabletop_squire/ChatBoxFragment.java
+++ app/src/main/java/edu/uwf/tabletopgroup/tabletop_squire/ChatBoxFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.github.nkzawa.socketio.client.*;
+
 
 
 /**
@@ -13,17 +17,24 @@ import android.view.ViewGroup;
  */
 public class ChatBoxFragment extends Fragment {
 
+    private EditText chatbox;
 
     public ChatBoxFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
+
+        chatbox = (EditText) getActivity().findViewById(R.id.chatbox);
+
+        //
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat_box, container, false);
+
     }
 
 }
--- app/src/main/java/edu/uwf/tabletopgroup/tabletop_squire/GameRoomActivity.java
+++ app/src/main/java/edu/uwf/tabletopgroup/tabletop_squire/GameRoomActivity.java
@@ -1,47 +1,75 @@
 package edu.uwf.tabletopgroup.tabletop_squire;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.EditText;
 
-public class GameRoomActivity extends Activity implements GestureDetector.OnGestureListener {
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.*;
+import com.github.nkzawa.socketio.client.IO;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class GameRoomActivity extends Activity implements Emitter.Listener {
+
+    private Socket connection;
+    {
+        try {
+            connection = IO.socket("localhost");
+        }
+        catch( URISyntaxException ex)
+        {
+
+        }
+    }
+
+    private EditText chattext;
+    private android.app.Fragment chatbox;
+    private Fragment userlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_room);
+
+        // Make the connection.....
+        connection.connect();
+
+        // Get references to our fragments for control ;)
+        chatbox = getFragmentManager().findFragmentById(R.id.chatbox_frag);
+        userlist = getFragmentManager().findFragmentById(R.id.userlist_frag);
+
+        // Direct access to the editbox, hopefully
+        chattext = (EditText) chatbox.getActivity().findViewById(R.id.chatbox);
+
+
+        // Begin processing messages from SocketIO
+        connection.on("message", this);
+
+        // set the view
+        setContentView(R.layout.game_room);
     }
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
+    public Socket getConnection()
+    {
+        return connection;
     }
 
+    /**
+     * Called in response to all SocketIO messages 'message'
+     * @param args
+     */
     @Override
-    public void onShowPress(MotionEvent e) {
+    public void call(Object... args) {
+        // Parse ellipted args, treat as JSON
+        JSONObject data = (JSONObject) args[0];
 
-    }
 
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
 
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
 
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY) {
-
-        return true;
     }
 }
